43563c2fed891f563cdf6548fda970ab3cf82e24
avalencia26 <avalencia26@student.rcc.edu>
2015-03-08 23:00:01 -0700

    Sum of Two Numbers Problem

---
+++ Hmwk/Assignment_1/Gaddis_7thEd_Chap2_Prob1/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Amilene Valencia
+ *Created on March 8, 2015, 09:46 AM
+ *  Purpose: Sum of Two Numbers Problem
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here!!
+int main(int argc, char** argv) {
+    unsigned char sxtyTwo, nntyNne, total;
+    
+    //Input Variables
+    sxtyTwo=62;
+    nntyNne=99;
+    total=sxtyTwo+nntyNne;
+    
+    cout<< "The sum of 62 and 99 is:"<<sxtyTwo+nntyNne<<endl;
+    return 0;
+}
+
