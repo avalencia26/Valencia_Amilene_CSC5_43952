121516469dc714dfb142799df90b264d5ed756dd
avalencia26 <avalencia26@student.rcc.edu>
2015-03-09 00:48:08 -0700

    Hmwk

---
+++ Hmwk/Assignment_1/Gaddis_7thEd_Chap2_Prob12/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Amilene Valencia
+ * Created on March 9, 2015, 12:36 AM
+ *      Purpose: Land Calculation
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution:
+int main(int argc, char** argv) {
+    //Declare Variables
+    unsigned short acre, pcofLnd;
+    //Known Values
+    acre=43560;//Square feet
+    pcofLnd=389767/acre;
+    
+    cout<<"There are "<<pcofLnd
+            <<" acres in a 389,767 square foot tract of land"<<endl;
+    return 0;
+}
+
