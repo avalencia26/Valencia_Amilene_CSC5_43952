95e31c05c8915f28336746300d5a16d7cabef4b6
avalencia26 <avalencia26@student.rcc.edu>
2015-03-09 00:14:56 -0700

    Problem 9

---
+++ Hmwk/Assignment_1/Gaddis_7thEd_Chap2_Prob9/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Amilene Valencia
+ * Created on March 8, 2015, 11:56 PM
+ *         Purpose: Cyborg Data Type Sizes
+ */
+
+//System Libararies
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+    cout<<"The size of a char data type is: "<< sizeof(char)<< " byte"<<endl;
+    cout<<"The size of a int data type is: "<< sizeof(int)<<" bytes"<<endl;
+    cout<<"The size of a float data type is: "<< sizeof(float)<<" bytes"<<endl;
+    cout<<"The size of a double data type is: "<< sizeof(double)<<" bytes"<<endl;
+    return 0;
+}
+
