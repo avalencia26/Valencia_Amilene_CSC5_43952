4048757f51ac249e26219706ff0c7491b8a0b26c
avalencia26 <avalencia26@student.rcc.edu>
2015-03-23 10:17:33 -0700

    Signed-off-by: avalencia26 <avalencia26@student.rcc.edu>

---
+++ Hmwk/Assignment_3/Gaddis_7thEd_Chap4_Prob1/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Amilene Valencia
+ * Created on March 23, 2015, 10:08 AM
+ *      Purpose: Minimum/Maximum Problem
+ * 
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    bool x, y;
+    //Input variables
+    cout<<"This program will determine which of the two\n"
+            "numbers is the larger and which one is the smaller.\n";
+    cout<<"Input the first number: "<<endl;
+    cin>>x;
+    cout<<"Input the second number: "<<endl;
+    cin>>y;
+    
+    
+    return 0;
+}
+
